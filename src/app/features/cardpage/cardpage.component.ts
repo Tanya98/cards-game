e7a2af41140a51030227a4df077777e20485bdcf
tanya290498 <29tatyana04@gmail.com>
2019-07-16 17:14:34 +0300

    unnecessary code has been removed

--- src/app/features/cardpage/cardpage.component.ts
+++ src/app/features/cardpage/cardpage.component.ts
@@ -1,67 +1,108 @@
-import {Component, ElementRef, OnInit, Renderer2, ViewChild, AfterViewInit} from '@angular/core';
+import {Component, OnInit, OnDestroy, ViewChild} from '@angular/core';
 import { CardsService } from '../../core/services/cards.service';
-import { Observable } from 'rxjs';
+import {interval, Observable, Subject, Subscription} from 'rxjs';
 import { Card } from '../../core/models/card';
-import {filter, map} from 'rxjs/operators';
-import {DataService} from "../../core/services/data.service";
+import { map, takeUntil} from 'rxjs/operators';
+import {LoginComponent} from "../account/login/login.component";
 
 @Component({
   selector: 'app-cardpage',
   templateUrl: './cardpage.component.html',
   styleUrls: ['./cardpage.component.scss']
 })
-export class CardpageComponent implements OnInit {
+export class CardpageComponent implements OnInit,OnDestroy {
   cards$: Observable<Card[]>;
-  multidimentionalArray = [];
-  text:string ="You're a winner!";
+  // multidimentionalArray = [];
   cards: Card[];
   activeCard: Card;
+  timer: number = 0;
+  stream: Observable<any>;
+  timerSubscription: Subscription;
+  cardsSubscription: Subscription;
+  stopPlay$: Subject<any> = new Subject();
+
+  @ViewChild(LoginComponent,{static:false})login:LoginComponent;
 
   constructor(
     private cardsService: CardsService
   ) {
+
     this.cards$ = this.cardsService.cards$;
-    this.cards$.pipe(
+    this.cardsSubscription = this.cards$.pipe(
       map((cards) => {
-        // debugger;
-        if(cards.length == 2){
-          return this.cardsService.showText(this.text);
+        this.cards = cards;
+        if (this.cards.length == 0) {
+          this.stopTimer();
+          this.countTicks();
         }
       })
     ).subscribe();
-    console.log(this.cards);
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.choosePlaygroundFirst();
-    // if(this.cards.length==2){
-    //   return this.cardsService.showText(this.text);
-    // }
-    // console.log(this.cards);
-
+    this.startTimer();
   }
 
   selectCard(card: Card) {
-    setTimeout(()=>{
+    setTimeout(() => {
       return this.cardsService.selectedCard(card);
-    },700)
+    }, 700)
   }
 
   choosePlaygroundFirst() {
-    return this.cardsService.getAllCardsPlaygroundFirst();
+    this.cardsService.getAllCardsPlaygroundFirst();
+    this.stopTimer();
+    this.countTicks();
+    this.startTimer();
   }
 
   choosePlaygroundSecond() {
     // debugger;
-    return this.cardsService.getAllCardsPlaygroundSecond();
+    this.cardsService.getAllCardsPlaygroundSecond();
+    this.stopTimer();
+    this.startTimer();
   }
 
   choosePlaygroundThird() {
     // debugger;
-    return this.cardsService.getAllCardsPlaygroundThird();
+    this.cardsService.getAllCardsPlaygroundThird();
+    this.stopTimer();
+    this.startTimer();
   }
 
   addClass(card) {
     this.activeCard = card;
+  }
+
+  startTimer() {
+    this.stream = interval(1000);
+    this.timerSubscription= this.stream.pipe(
+      takeUntil(this.stopPlay$),
+    ).subscribe(v=> this.timer = v);
+  }
+
+  stopTimer(){
+    // console.log(this.timer);
+    this.stopPlay$.next();
+    this.countTicks();
+    this.timer = 0;
+  }
+
+  ngOnDestroy() {
+    // this.stopTimer();
+    this.timerSubscription.unsubscribe();
+    this.cardsSubscription.unsubscribe();
+  }
+
+  countTicks(){
+    let userInfo = localStorage.getItem('userInfo');
+    let info = JSON.parse(userInfo);
+    if(this.timer!==0){
+      info.timer = this.timer;
+      let user = JSON.stringify(info);
+      localStorage.setItem('userInfo',user);
+      console.log(user);
+    }
   }
 }
