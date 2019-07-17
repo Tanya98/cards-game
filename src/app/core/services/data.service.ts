c222555e6ae61e88518dbe7ab034070a770bfcfd
tanya290498 <29tatyana04@gmail.com>
2019-07-17 17:41:55 +0300

    some changes in data base

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -4,22 +4,27 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({ providedIn: 'root' })
 
 export class DataService {
-  private playground1 = 'api/playground1';
-  private playground2 = 'api/playground2';
-  private playground3 = 'api/playground3';
+  // private playground1 = 'api/playground1';
+  // private playground2 = 'api/playground2';
+  // private playground3 = 'api/playground3';
+  private playground = 'api/playground';
 
   constructor(public http: HttpClient) { }
 
-  public getPlayground1() {
-    return this.http.get(this.playground1);
-  }
+  // public getPlayground1() {
+  //   return this.http.get(this.playground1);
+  // }
+  //
+  // public getPlayground2() {
+  //   return this.http.get(this.playground2);
+  // }
+  //
+  // public getPlayground3() {
+  //   return this.http.get(this.playground3);
+  // }
 
-  public getPlayground2() {
-    return this.http.get(this.playground2);
-  }
-
-  public getPlayground3() {
-    return this.http.get(this.playground3);
+  public getCards(){
+    return this.http.get(this.playground);
   }
 
 }
