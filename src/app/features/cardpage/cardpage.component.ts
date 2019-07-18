cd496d57642e88cfc3286ca7cbcea306aa2db797
tanya290498 <29tatyana04@gmail.com>
2019-07-18 17:55:55 +0300

    some new features have been done for selecting card and for tracking time

--- src/app/features/cardpage/cardpage.component.ts
+++ src/app/features/cardpage/cardpage.component.ts
@@ -2,41 +2,57 @@ import { Component, OnInit, OnDestroy, ViewChild, DoCheck, AfterViewInit } from 
 import { CardsService } from '../../core/services/cards.service';
 import { interval, Observable, Subject, Subscription } from 'rxjs';
 import { Card } from '../../core/models/card';
-import { takeUntil, tap } from 'rxjs/operators';
+import {filter, map, takeUntil, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-cardpage',
   templateUrl: './cardpage.component.html',
   styleUrls: ['./cardpage.component.scss']
 })
+
 export class CardpageComponent implements OnInit, OnDestroy {
   cards$: Observable<Card[]>;
+  selectedCards$: Observable<Card[]>;
   cards: Card[];
-  playground: Card[];
   activeCard: Card;
   timer: number = 0;
   stream: Observable<any>;
+  getEventTarget:any;
   timerSubscription: Subscription;
   cardsSubscription: Subscription;
+  selectedCardsSubscription: Subscription;
   stopPlay$: Subject<any> = new Subject();
 
   constructor(
     private cardsService: CardsService
   ) {
     this.cards$ = this.cardsService.cards$;
+    this.selectedCards$ = this.cardsService.selectedCard$;
     this.cardsSubscription = this.cards$.pipe(
       tap((cards) => {
         this.cards = cards;
-        if (this.cards.length === 0) {
+        if (!this.cards.length) {
           this.stopTimer();
-          this.countTicks();
+          // this.countTicks();
         }
       })
     ).subscribe();
   }
 
   ngOnInit() {
-    this.easyMode();
+    const _4X4 = 16;
+    this.cardsService.getCards(_4X4);
+    this.changeMode();
+
+    this.selectedCardsSubscription = this.selectedCards$.pipe(
+      filter(sl=> !!sl[0]&&!!sl[1])
+    ).subscribe(sl => {
+        if(sl[0].pairId == sl[1].pairId){
+          return this.cardsService.deleteCards();
+        } else {
+          return this.cardsService.clear();
+        }
+      })
   }
 
   selectCard(card: Card) {
@@ -45,22 +61,25 @@ export class CardpageComponent implements OnInit, OnDestroy {
     }, 700);
   }
 
-  easyMode() {
+  easyMode(event) {
     const _4X4 = 16;
     this.cardsService.getCards(_4X4);
     this.changeMode();
+    this.getEventTarget = event.target.id;
   }
 
-  mediumMode() {
+  mediumMode(event) {
     const _6X6 = 36;
     this.cardsService.getCards(_6X6);
     this.changeMode();
+    this.getEventTarget = event.target.id;
   }
 
-  hardMode() {
+  hardMode(event) {
     const _8X8 = 64;
     this.cardsService.getCards(_8X8);
     this.changeMode();
+    this.getEventTarget = event.target.id;
   }
 
   changeMode(){
@@ -87,28 +106,29 @@ export class CardpageComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.timerSubscription.unsubscribe();
+    // this.timerSubscription.unsubscribe();
     this.cardsSubscription.unsubscribe();
+    this.selectedCardsSubscription.unsubscribe();
   }
 
   countTicks() {
     const userInfo = localStorage.getItem('userInfo');
     const info = JSON.parse(userInfo);
     if (this.timer !== 0) {
-
-      if (this.cards.length === 16) {
+    debugger;
+      if (this.getEventTarget === '1') {
         info.timerPlaygroundFirst = this.timer;
         const user = JSON.stringify(info);
         localStorage.setItem('userInfo', user);
       }
 
-      if (this.cards.length === 36) {
+      if (this.getEventTarget === '2') {
         info.timerPlaygroundSecond = this.timer;
         const user = JSON.stringify(info);
         localStorage.setItem('userInfo', user);
       }
 
-      if (this.cards.length === 64) {
+      if (this.getEventTarget === '3') {
         info.timerPlaygroundThird = this.timer;
         const user = JSON.stringify(info);
         localStorage.setItem('userInfo', user);
@@ -118,17 +138,18 @@ export class CardpageComponent implements OnInit, OnDestroy {
   }
 
   addUser() {
-    debugger;
     let getUser = JSON.parse(localStorage.getItem('userInfo'));
     let getAllInfo = JSON.parse(localStorage.getItem('allInfo'));
 
     if (getAllInfo.length === 0) {
+      debugger;
       getAllInfo.push(getUser);
       let newUser = JSON.stringify(getAllInfo);
       localStorage.setItem('allInfo', newUser);
     }
 
     getAllInfo.forEach(user => {
+      debugger;
       if (user.email === getUser.email) {
         let deleteUser = getAllInfo.filter(x => x.email !== getUser.email);
         deleteUser.push(getUser);
